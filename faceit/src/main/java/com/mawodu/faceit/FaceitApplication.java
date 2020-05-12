775ceb76a2547f785a519571931209dceaf27117
MaWoDu <durkiewiczmaciej@gmail.com>
2020-05-12 21:42:00 +0100

    init - generated boilerplate for spring app.

---
+++ faceit/src/main/java/com/mawodu/faceit/FaceitApplication.java
@@ -0,0 +1,13 @@
+package com.mawodu.faceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceitApplication.class, args);
+	}
+
+}
